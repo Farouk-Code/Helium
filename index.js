4aebbb5d78a7f21bf41d25e80dd3b848dd02d96b
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-05-13 01:21:39 +0200

    Alternative

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
 
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
@@ -6,15 +7,21 @@
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
 
-const mouse2 = document.getElementById("mouse2");
-const mouse1 = document.getElementById("mouse1");
-const cursor = document.getElementById("cursor");
+// const mouse2 = document.getElementById("mouse2");
+// const mouse1 = document.getElementById("mouse1");
+// const cursor = document.getElementById("cursor");
+
+const cursors = document.querySelectorAll(".mouse");
 
 window.addEventListener("mousemove", (e) => {
-  cursor.style.left = `${e.pageX}px`;
-  cursor.style.top = `${e.pageY}px`;
-  mouse1.style.left = `${e.pageX}px`;
-  mouse1.style.top = `${e.pageY}px`;
-  mouse2.style.left = `${e.pageX}px`;
-  mouse2.style.top = `${e.pageY}px`;
+  for (const cursor of cursors) {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
+  }
+  // cursor.style.left = `${e.pageX}px`;
+  // cursor.style.top = `${e.pageY}px`;
+  // mouse1.style.left = `${e.pageX}px`;
+  // mouse1.style.top = `${e.pageY}px`;
+  // mouse2.style.left = `${e.pageX}px`;
+  // mouse2.style.top = `${e.pageY}px`;
 });
